945ac067bdbe8726eda62f2aea2784c46ad1ee51
Sergy93 <takanuva7493@hotmail.com>
2015-05-29 10:53:07 +0200

    puresupuestos acabados

--- WebApplication2/scripts/index.js
+++ WebApplication2/scripts/index.js
@@ -4,21 +4,43 @@
         alert("Bienvenido!");
     }, 4000);
 
-    //There's only one text to display so it's always displayed the same,
-    //but in case we need to change we would separate the text in different html files and load them separately
-    $("#right > .text:first-of-type").load("SubmenuContent.html");
+
+    //The toggle the showing of comments
+    $("#comLink").click(function () {
+        $("#comentarios").slideToggle();
+    });
 
     //Get the submenu elements we are gonna turn into ajax calls
+    $("#right > .text:first-of-type").load("Submenu1.html");
+
     var linksOfSubmenu = document.querySelector("#right > h4");
 
     for (var key in linksOfSubmenu.children) {
-        var a = linksOfSubmenu.children[key];
+        var anchor = linksOfSubmenu.children[key];
 
-        a.href = "#";
-        a.onclick = function (event) {
+        anchor.href = "#";
+
+        anchor.onclick = function (event) {
             event.preventDefault();
-            $("#right > .text:first-of-type").load("SubmenuContent.html");
-
-        }
+            var fileToLoad;
+            switch (event.target.innerHTML) {
+                case "Metodología":
+                    fileToLoad = "Submenu2.html";
+                    break;
+                case "Historia":
+                    fileToLoad = "Submenu3.html";
+                    break;
+                case "Personal":
+                    fileToLoad = "Submenu4.html";
+                    break;
+                default:
+                    fileToLoad = "Submenu1.html";
+                    break;
+            }
+            $("#right > .text:first-of-type").load(fileToLoad);
+        };
     };
+
+
+
 })();
--- WebApplication2/scripts/presupuesto.js
+++ WebApplication2/scripts/presupuesto.js
@@ -1,1 +1,52 @@
-﻿
+﻿(function () {
+
+    var finalPrice = 0;
+
+    var webPrice = 0;
+    var monthsDiscount = 0;
+    var functionsCharge = 0;
+
+    function calculateTotal() {
+        monthsDiscount *= 5;
+        if (monthsDiscount > 20) monthsDiscount = 20;
+
+        if (functionsCharge < 0) functionsCharge = 0;
+
+        finalPrice = (webPrice *(1-(monthsDiscount /100)) +functionsCharge);
+
+        $("#presupuesto").val(finalPrice);
+    }
+
+
+    $("#tipoWeb").change(function() {
+        switch($("#tipoWeb option:selected").attr('id')) {
+        case "empresarial":
+            webPrice = 2000;
+            break;
+        case "personal":
+            webPrice = 1000;
+            break;
+        default:
+            webPrice = 500;
+            break;
+        }
+        calculateTotal();
+        });
+
+    $("#plazo").change(function() {
+
+        if(!isNaN($(this).val())) {
+            monthsDiscount = this.value;
+            calculateTotal();
+        }
+    });
+
+    $("input[type='checkbox']").click(function () {
+        if ($(this).prop("checked")) functionsCharge += 400;
+        else functionsCharge -= 400;
+
+        calculateTotal();
+
+});
+})();
+
